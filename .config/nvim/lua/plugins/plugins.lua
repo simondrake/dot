d8acdaa28b51ff56e149fca24131364bc45da9bc
Simon Drake <sdrake@splunk.com>
2023-05-18 12:49:19 +0100

    remove nvim-navbuddy

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -19,17 +19,6 @@ require('packer').startup(function(use)
     }
   end
   }
-
-  use {
-    "SmiteshP/nvim-navbuddy",
-    requires = {
-        "neovim/nvim-lspconfig",
-        "SmiteshP/nvim-navic",
-        "MunifTanjim/nui.nvim",
-        "numToStr/Comment.nvim",        -- Optional
-        "nvim-telescope/telescope.nvim" -- Optional
-    }
-  }
   -- }
 
   -- Git {
